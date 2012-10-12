f49c7a1a3db43270da75039cff020fa117369c73
Ben Merritt <blm768@gmail.com>
2012-10-12 02:11:23 +0200

    Started unit tests for TORQUE client

--- bookie-torque_client/lib/bookie-torque_client/client.rb
+++ bookie-torque_client/lib/bookie-torque_client/client.rb
@@ -1,15 +1,18 @@
+require 'bookie'
 require 'bookie-torque_client'
 
 require 'date'
 require 'torque_stats'
 
 module Bookie
+  module TorqueClient
   #Represents a client that returns data from a TORQUE cluster
-  class TorqueClient < Bookie::Client
-    def send_data(date = Date.yesterday)
-      record = TorqueStats::JobRecord.new(date)
-      record.each_job do |job|
-        puts filter_job(job)
+    class Client < Bookie::Client
+      def send_data(date = Date.yesterday)
+        record = TorqueStats::JobRecord.new(date)
+        record.each_job do |job|
+          puts filter_job(job)
+        end
       end
     end
   end
