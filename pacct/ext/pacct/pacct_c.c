3814a957f0480c5921584633c541adb1fa157085
Ben Merritt <blm768@gmail.com>
2012-10-30 23:13:06 +0100

    Improved pacct to support setting fields

--- pacct/ext/pacct/pacct_c.c
+++ pacct/ext/pacct/pacct_c.c
@@ -26,6 +26,7 @@ VALUE cSystemCallError;
 //Identifiers
 ID id_at;
 ID id_new;
+ID id_to_i;
 
 //System parameters
 int pageSize;
@@ -36,9 +37,26 @@ long comp_t_to_long(comp_t c) {
   return (c & 0x1fff) << (((c >> 13) & 0x7) * 3);
 }
 
-//Converts a comp_t to a Ruby number
-VALUE comp_t_to_num(comp_t c) {
-  return INT2NUM(comp_t_to_long(c));
+//Converts a long to a comp_t
+//To do: make sure the value is positive?
+comp_t long_to_comp_t(long l) {
+  size_t bits = 0;
+  unsigned long l2 = l;
+  while(l2 >>= 1) {
+    ++bits;
+  }
+  if(bits <= 13) {
+    return (l & 0x1fff);
+  } else {
+    size_t div_bits, rem_bits;
+    bits -= 13;
+    div_bits = bits / 3;
+    rem_bits = bits - div_bits * 3;
+    if(rem_bits) {
+      div_bits += 1;
+    }
+    return (l >> (bits + rem_bits) & 0x1fff) | ((div_bits & 0x7) << 13);
+  }
 }
 
 //Called when an exception is thrown in the block passed to each_entry()
@@ -126,6 +144,10 @@ static VALUE pacct_entry_new(PacctFile* file) {
   entry = Data_Wrap_Struct(cEntry, 0, free, ptr);
   
   return entry;
+}
+
+static VALUE ruby_pacct_entry_new(VALUE self) {
+  return pacct_entry_new(NULL);
 }
 
 /*
@@ -274,11 +296,36 @@ static VALUE get_user_name(VALUE self) {
     char buf[512];
     VALUE err;
     snprintf(buf, 512, "Unable to obtain user name for ID %u", data->ac_uid);
+    //To do: clearer messages when errno == 0?
     err = rb_funcall(cSystemCallError, id_new, 2, rb_str_new2(buf), INT2NUM(errno));
     rb_exc_raise(err);
   }
   
   return rb_str_new2(pw_data->pw_name);
+}
+
+/*
+ *Sets the name of the user who executed the command
+ */
+static VALUE set_user_name(VALUE self, VALUE name) {
+  struct acct_v3* data;
+  struct passwd* pw_data;
+  char* cName = StringValueCStr(name);
+  Data_Get_Struct(self, struct acct_v3, data);
+  
+  errno = 0;
+  pw_data = getpwnam(cName);
+  if(!pw_data) {
+    char buf[512];
+    VALUE err;
+    snprintf(buf, 512, "Unable to obtain user ID for name %s", cName);
+    err = rb_funcall(cSystemCallError, id_new, 2, rb_str_new2(buf), INT2NUM(errno));
+    rb_exc_raise(err);
+  }
+  
+  data->ac_uid = pw_data->pw_uid;
+  
+  return Qnil;
 }
 
 /*
@@ -313,6 +360,30 @@ static VALUE get_group_name(VALUE self) {
 }
 
 /*
+ *Sets the group name of the user who executed the command
+ */
+static VALUE set_group_name(VALUE self, VALUE name) {
+  struct acct_v3* data;
+  struct group* group_data;
+  char* cName = StringValueCStr(name);
+  Data_Get_Struct(self, struct acct_v3, data);
+  
+  errno = 0;
+  group_data = getgrnam(cName);
+  if(!group_data) {
+    char buf[512];
+    VALUE err;
+    snprintf(buf, 512, "Unable to obtain user ID for name %s", cName);
+    err = rb_funcall(cSystemCallError, id_new, 2, rb_str_new2(buf), INT2NUM(errno));
+    rb_exc_raise(err);
+  }
+  
+  data->ac_gid = group_data->gr_gid;
+  
+  return Qnil;
+}
+
+/*
  *Returns the task's total user CPU time in seconds
  */
 static VALUE get_user_time(VALUE self) {
@@ -323,6 +394,18 @@ static VALUE get_user_time(VALUE self) {
 }
 
 /*
+ *Sets the task's total user CPU time
+ */
+static VALUE set_user_time(VALUE self, VALUE value) {
+  struct acct_v3* data;
+  Data_Get_Struct(self, struct acct_v3, data);
+  
+  data->ac_utime = long_to_comp_t(NUM2LONG(value) * ticksPerSecond);
+  
+  return Qnil;
+}
+
+/*
  *Returns the task's total system CPU time in seconds
  */
 static VALUE get_system_time(VALUE self) {
@@ -330,6 +413,18 @@ static VALUE get_system_time(VALUE self) {
   Data_Get_Struct(self, struct acct_v3, data);
   
   return INT2NUM(comp_t_to_long(data->ac_stime) / ticksPerSecond);
+}
+
+/*
+ *Sets the task's total system CPU time
+ */
+static VALUE set_system_time(VALUE self, VALUE value) {
+  struct acct_v3* data;
+  Data_Get_Struct(self, struct acct_v3, data);
+  
+  data->ac_stime = long_to_comp_t(NUM2LONG(value) * ticksPerSecond);
+  
+  return Qnil;
 }
 
 /*
@@ -353,6 +448,18 @@ static VALUE get_wall_time(VALUE self) {
 }
 
 /*
+ *Sets the task's total wall time
+ */
+static VALUE set_wall_time(VALUE self, VALUE value) {
+  struct acct_v3* data;
+  Data_Get_Struct(self, struct acct_v3, data);
+  
+  data->ac_etime = NUM2DBL(value);
+  
+  return Qnil;
+}
+
+/*
  *Returns the task's start time
  */
 static VALUE get_start_time(VALUE self) {
@@ -360,6 +467,18 @@ static VALUE get_start_time(VALUE self) {
   Data_Get_Struct(self, struct acct_v3, data);
   
   return rb_funcall(cTime, id_at, 1, INT2NUM(data->ac_btime));
+}
+
+/*
+ *Sets the task's start time
+ */
+static VALUE set_start_time(VALUE self, VALUE value) {
+  struct acct_v3* data;
+  Data_Get_Struct(self, struct acct_v3, data);
+  
+  data->ac_btime = NUM2INT(rb_funcall(value, id_to_i, 0));
+  
+  return Qnil;
 }
 
 /*
@@ -374,13 +493,38 @@ static VALUE get_average_mem_usage(VALUE self) {
 }
 
 /*
- *Returns the first 16 characters of the command name
+ *Sets the task's average memory usage in kilobytes
+ */
+static VALUE set_average_mem_usage(VALUE self, VALUE value) {
+  struct acct_v3* data;
+  Data_Get_Struct(self, struct acct_v3, data);
+  
+  data->ac_mem = long_to_comp_t(NUM2LONG(value) * pageSize / 1024);
+  
+  return Qnil;
+}
+
+/*
+ *Returns the first 15 characters of the command name
  */
 static VALUE get_command_name(VALUE self) {
   struct acct_v3* data;
   Data_Get_Struct(self, struct acct_v3, data);
   
   return rb_str_new2(data->ac_comm);
+}
+
+/*
+ *Sets the first 15 characters of the command name
+ */
+static VALUE set_command_name(VALUE self, VALUE name) {
+  struct acct_v3* data;
+  Data_Get_Struct(self, struct acct_v3, data);
+  
+  strncpy(data->ac_comm, StringValueCStr(name), ACCT_COMM - 1);
+  data->ac_comm[ACCT_COMM - 1] = '\0';
+  
+  return Qnil;
 }
 
 /*
@@ -393,6 +537,18 @@ static VALUE get_exit_code(VALUE self) {
   return INT2NUM(data->ac_exitcode);
 }
 
+/*
+ *Sets the command's exit code
+ */
+static VALUE set_exit_code(VALUE self, VALUE value) {
+  struct acct_v3* data;
+  Data_Get_Struct(self, struct acct_v3, data);
+  
+  data->ac_exitcode = NUM2INT(value);
+  
+  return Qnil;
+}
+
 void Init_pacct_c() {
   //Get system parameters
   pageSize = getpagesize();
@@ -403,6 +559,7 @@ void Init_pacct_c() {
   cSystemCallError = rb_eval_string("SystemCallError");
   id_at = rb_intern("at");
   id_new = rb_intern("new");
+  id_to_i = rb_intern("to_i");
 
   //Define Ruby modules/objects/methods.
   mPacct = rb_define_module("Pacct");
@@ -423,17 +580,28 @@ void Init_pacct_c() {
   rb_define_method(cFile, "num_entries", get_num_entries, 0);
   rb_define_method(cFile, "write_entry", write_entry, 1);
   
+  rb_define_singleton_method(cEntry, "new", ruby_pacct_entry_new, 0);
   rb_define_method(cEntry, "process_id", get_process_id, 0);
+  rb_define_method(cEntry, "process_id=", set_process_id, 1);
   rb_define_method(cEntry, "user_id", get_user_id, 0);
   rb_define_method(cEntry, "user_name", get_user_name, 0);
+  rb_define_method(cEntry, "user_name=", set_user_name, 1);
   rb_define_method(cEntry, "group_id", get_group_id, 0);
   rb_define_method(cEntry, "group_name", get_group_name, 0);
+  rb_define_method(cEntry, "group_name=", set_group_name, 1);
   rb_define_method(cEntry, "user_time", get_user_time, 0);
+  rb_define_method(cEntry, "user_time=", set_user_time, 1);
   rb_define_method(cEntry, "system_time", get_system_time, 0);
+  rb_define_method(cEntry, "system_time=", set_system_time, 1);
   rb_define_method(cEntry, "cpu_time", get_cpu_time, 0);
   rb_define_method(cEntry, "wall_time", get_wall_time, 0);
+  rb_define_method(cEntry, "wall_time=", set_wall_time, 1);
   rb_define_method(cEntry, "start_time", get_start_time, 0);
+  rb_define_method(cEntry, "start_time=", set_start_time, 1);
   rb_define_method(cEntry, "memory", get_average_mem_usage, 0);
+  rb_define_method(cEntry, "memory=", set_average_mem_usage, 1);
   rb_define_method(cEntry, "exit_code", get_exit_code, 0);
+  rb_define_method(cEntry, "exit_code=", set_exit_code, 1);
   rb_define_method(cEntry, "command_name", get_command_name, 0);
+  rb_define_method(cEntry, "command_name=", set_command_name, 1);
 }
