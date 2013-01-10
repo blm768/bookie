5e04b3c98d5e2e98ab6a9e587521b50690a6bf5f
Ben Merritt <blm768@gmail.com>
2013-01-10 15:39:02 -0800

    Significantly improved filter interface functionality

--- bookie-ui/app/assets/javascripts/jobs.js
+++ bookie-ui/app/assets/javascripts/jobs.js
@@ -1,25 +1,68 @@
 function nextElement(node) {
   var next = node.nextSibling
-  while(next.nodeType != 1) {
+  while(next && next.nodeType != 1) {
     next = next.nextSibling
   }
   return next
 }
 
-//Sets the visibility of a filter's "extra" fields
-function setFormExtrasVisibility(filter) {
-  var opt = filter.options[filter.selectedIndex]
-  var filter_extras = nextElement(filter)
-  if(opt.value == 'none') {
-    filter_extras.style.display = 'none'
-    document.getElementById(filter.id + '_value').value = ''
-  } else {
-    filter_extras.style.display = 'inline'
+var filterMax = 0
+
+function forEachFilter(action) {
+  var filters = document.getElementById('filters')
+  var filter = filters.firstChild
+  while(filter && filter.nodeType != 1) {
+    filter = filter.nextSibling
+  }
+  while(filter) {
+    action(filter)
+    filter = nextElement(filter)
   }
 }
 
+function addFilter() {
+  var select = document.getElementById('add_filter')
+  if(select.selectedIndex == 0) {
+    return
+  }
+  var opt = select.options[select.selectedIndex]
+  select.selectedIndex = 0
+  
+  var filters = document.getElementById('filters')
+  var filter = document.createElement('div')
+  filter.class = 'filter'
+  filter.id = 'filter' + filterMax
+  filter.appendChild(document.createTextNode(opt.firstChild.nodeValue))
+  var text = document.createElement('input')
+  text.type = 'text'
+  filter.appendChild(text)
+  var remover = document.createElement('div')
+  remover.class = 'filter_remover'
+  remover.onclick = function() { filters.removeChild(filter) }
+  remover.appendChild(document.createTextNode("X"))
+  filter.appendChild(remover)
+  filters.appendChild(filter)
+  ++filterMax
+}
+
+function submitFilters() {
+  var filters = document.getElementById('filters')
+  var filterForm = filters.parentNode
+  filterTypes = []
+  forEachFilter(function(filter) {
+    filterTypes.push(filter.firstChild.nodeValue)
+  })
+  var filterTypesInput = document.createElement('input')
+  filterTypesInput.type = 'hidden'
+  filterTypesInput.name = 'filter_types'
+  filterTypesInput.value = filterTypes.join(',')
+  filterForm.appendChild(filterTypesInput)
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-  var filter0 = document.getElementById('filter0')
+  var addFilterSelect = document.getElementById('add_filter')
   //This should be addEventListener(), but Safari doesn't like that. I have no idea why.
-  filter0.onchange = function() { setFormExtrasVisibility(filter0) }
+  addFilterSelect.onchange = addFilter
+  var filterForm = addFilterSelect.parentNode
+  filterForm.onsubmit = submitFilters
 }, false);
