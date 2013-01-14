b0a384a3c21fb5e178b1e4d97cd628382ae87bfd
Ben Merritt <blm768@gmail.com>
2013-01-14 11:21:29 -0800

    Stylesheet tweak; started working on using JQuery

--- bookie-ui/app/assets/javascripts/filter.js
+++ bookie-ui/app/assets/javascripts/filter.js
@@ -8,75 +8,62 @@ function nextElement(node) {
 
 var filterMax = 0
 
-function forEachFilter(action) {
-  var filters = document.getElementById('filters')
-  var filter = filters.firstChild
-  while(filter && filter.nodeType != 1) {
-    filter = filter.nextSibling
-  }
-  while(filter) {
-    action(filter)
-    filter = nextElement(filter)
-  }
-}
-
 function addFilter() {
-  var select = document.getElementById('add_filter')
-  if(select.selectedIndex == 0) {
+  var select = $('#add_filter')
+  if(select.val() == 0) {
     return
   }
-  var opt = select.options[select.selectedIndex]
-  select.selectedIndex = 0
+  var opt = select.children(':selected')
+  select.val(0)
   
-  var filters = document.getElementById('filters')
-  var filter = document.createElement('div')
-  filter.class = 'filter'
-  filter.id = 'filter' + filterMax
-  filter.appendChild(document.createTextNode(opt.firstChild.nodeValue))
-  var text = document.createElement('input')
-  text.type = 'text'
-  filter.appendChild(text)
-  var remover = document.createElement('div')
-  remover.class = 'filter_remover'
-  remover.onclick = function() { filters.removeChild(filter) }
-  remover.appendChild(document.createTextNode("X"))
-  filter.appendChild(remover)
-  filters.appendChild(filter)
+  var filters = $('#filters')
+  var filter = $('<div/>')
+  filter.addClass('filter')
+  filter.append(opt.text())
+  //The value attribute is hijacked to store the number of filter parameters.
+  for(var i = 0; i < parseInt(opt.val()); ++i) {
+    var text = $('<input/>')
+    text.type = 'text'
+    filter.append(text)
+  }
+  var remover = $('<div/>')
+  remover.addClass('filter_remover')
+  remover.click(function() { filter.remove() })
+  remover.append('X')
+  filter.append(remover)
+  filters.append(filter)
   ++filterMax
 }
 
 function submitFilters() {
-  var filters = document.getElementById('filters')
-  var filterForm = filters.parentNode
+  var filters = $('#filters')
+  var filterForm = filters.parent()
   var filterTypes = []
   var filterValues = []
-  forEachFilter(function(filter) {
-    var textNode = filter.firstChild
-    filterTypes.push(textNode.nodeValue)
-    var valueNode = nextElement(textNode)
-    while(valueNode && valueNode.tagName == 'input') {
-      alert(valueNode.value)
-      filterValues.push(valueNode.value)
-      valueNode = nextElement(valueNode)
-    }
+  filters.children('.filter').each(function() {
+    var text = this.children(':first').text()
+    filterTypes.push(text)
+    this.children('input').each(function() {
+      filterValues.push(this.text())
+    })
   })
-  var filterTypesInput = document.createElement('input')
+  var filterTypesInput = $('<input/>')
   filterTypesInput.type = 'hidden'
   filterTypesInput.name = 'filter_types'
-  filterTypesInput.value = filterTypes.join(',')
-  filterForm.appendChild(filterTypesInput)
-  var filterValuesInput = document.createElement('input')
+  filterTypesInput.val(filterTypes.join(','))
+  filterForm.append(filterTypesInput)
+  var filterValuesInput = $('<input/>')
   filterValuesInput.type = 'hidden'
   filterValuesInput.name = 'filter_values'
   //To do: prevent commas in the values from causing problems.
-  filterValuesInput.value = filterValues.join(',')
-  filterForm.appendChild(filterValuesInput)
+  filterValuesInput.val(filterValues.join(','))
+  filterForm.append(filterValuesInput)
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  var addFilterSelect = document.getElementById('add_filter')
+$(document).ready(function() {
+  var addFilterSelect = $('#add_filter')
   //This should be addEventListener(), but Safari doesn't like that. I have no idea why.
-  addFilterSelect.onchange = addFilter
-  var filterForm = addFilterSelect.parentNode
-  filterForm.onsubmit = submitFilters
-}, false);
+  addFilterSelect.change(addFilter)
+  var filterForm = addFilterSelect.parent()
+  filterForm.submit(submitFilters)
+})
