e09e750ffec552584a1be34a21aa8075e83dfbf6
Ben Merritt <blm768@gmail.com>
2013-01-14 12:24:42 -0800

    Added "details" checkbox; started working on form persistence

--- bookie-ui/app/controllers/jobs_controller.rb
+++ bookie-ui/app/controllers/jobs_controller.rb
@@ -16,7 +16,10 @@ class JobsController < ApplicationController
     types = params[:filter_types]
     values = params[:filter_values]
     
-    #To do: error messages
+    #Passed to the view to make the filter form's contents persistent
+    @prev_filters = []
+    
+    #To do: error on empty fields?
     if types && values
       types.strip!
       types = types.split(',')
@@ -30,14 +33,20 @@ class JobsController < ApplicationController
           jobs = jobs.by_system_name(value)
           systems = systems.by_name(value)
           value_index += 1
+          @prev_filters << ['System', [value]]
         when 'User'
-          jobs = jobs.by_user_name(values[value_index])
+          value = values[value_index]
+          jobs = jobs.by_user_name(value)
           value_index += 1
+          @prev_filters << ['User', [value]]
         when 'Group'
-          jobs = jobs.by_group_name(values[value_index])
+          value = values[value_index]
+          jobs = jobs.by_group_name(value)
           value_index += 1
+          @prev_filters << ['Group', [value]]
         when 'System type'
-          sys_type = Bookie::Database::SystemType.find_by_name(values[value_index])
+          value = values[value_index]
+          sys_type = Bookie::Database::SystemType.find_by_name(value)
           if sys_type
             jobs = jobs.by_system_type(sys_type)
             systems = systems.by_system_type(sys_type)
@@ -46,10 +55,22 @@ class JobsController < ApplicationController
             systems = systems.limit(0)
           end
           value_index += 1
+          @prev_filters << ['System type', [value]]
         when 'Time'
-          summmary_start_time = Time.parse(values[value_index])
-          summary_end_time = Time.parse(values[value_index + 1])
+          start_time_text = values[value_index]
+          end_time_text = values[value_index + 1]
+          begin
+            summmary_start_time = Time.parse(start_time_text)
+          rescue
+            flash[:error] = "Invalid start time '#{start_time_text}'"
+          end
+          begin
+            summary_end_time = Time.parse(end_time_text)
+          rescue
+            flash[:error] = "Invalid end time '#{end_time_text}'"
+          end
           value_index += 2
+          @prev_filters << ['Time', [start_time_text, end_time_text]]
         end
       end
     end
@@ -57,6 +78,11 @@ class JobsController < ApplicationController
     #To do: ordering?
     @jobs_summary = jobs.summary(summary_start_time, summary_end_time)
     @systems_summary = systems.summary(summary_start_time, summary_end_time)
+    
+    #To be passed to the view
+    @filter_types = types
+    @filter_values = values
+    @include_details = (params[:details] == "true")
     
     render :template => 'jobs/index'
   end
