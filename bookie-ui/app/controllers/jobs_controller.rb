d9b85d1ebb8f526681169e07facae02b6e68e4fb
Ben Merritt <blm768@gmail.com>
2013-01-15 15:00:10 -0800

    Started work on consolidating filter code

--- bookie-ui/app/controllers/jobs_controller.rb
+++ bookie-ui/app/controllers/jobs_controller.rb
@@ -1,11 +1,19 @@
 require 'date'
 
-class BadDateError < RangeError
-
-end
+require 'filter'
 
 class JobsController < ApplicationController
   PAGE_SIZE = 20
+  
+  FILTER_ARG_COUNTS = {
+    'System' => 1,
+    'User' => 1,
+    'Group' => 1,
+    'System type' => 1,
+    'Time' => 2
+  }
+  
+  include FilterMixin
 
   def index
     #To do: optimize as local variable?
@@ -15,8 +23,6 @@ class JobsController < ApplicationController
     summary_start_time = nil
     summary_end_time = nil
     
-    types = params[:filter_types]
-    values = params[:filter_values]
     @page_num = params[:page].to_i
     @page_num = 1 unless @page_num && @page_num > 0
         
@@ -24,58 +30,45 @@ class JobsController < ApplicationController
     @prev_filters = []
     
     #To do: error on empty fields?
-    if types && values
-      types.strip!
-      types = types.split(',')
-      values.strip!
-      values = values.split(',')
-      value_index = 0
-      types.each do |type|
-        case type
-        when 'System'
-          value = values[value_index]
-          jobs = jobs.by_system_name(value)
-          systems = systems.by_name(value)
-          value_index += 1
-          @prev_filters << ['System', [value]]
-        when 'User'
-          value = values[value_index]
-          jobs = jobs.by_user_name(value)
-          value_index += 1
-          @prev_filters << ['User', [value]]
-        when 'Group'
-          value = values[value_index]
-          jobs = jobs.by_group_name(value)
-          value_index += 1
-          @prev_filters << ['Group', [value]]
-        when 'System type'
-          value = values[value_index]
-          sys_type = Bookie::Database::SystemType.find_by_name(value)
-          if sys_type
-            jobs = jobs.by_system_type(sys_type)
-            systems = systems.by_system_type(sys_type)
-          else
-            jobs = jobs.limit(0)
-            systems = systems.limit(0)
-          end
-          value_index += 1
-          @prev_filters << ['System type', [value]]
-        when 'Time'
-          start_time_text = values[value_index]
-          end_time_text = values[value_index + 1]
-          begin
-            summmary_start_time = Time.parse(start_time_text)
-          rescue
-            flash[:error] = "Invalid start time '#{start_time_text}'"
-          end
-          begin
-            summary_end_time = Time.parse(end_time_text)
-          rescue
-            flash[:error] = "Invalid end time '#{end_time_text}'"
-          end
-          value_index += 2
-          @prev_filters << ['Time', [start_time_text, end_time_text]]
+    each_filter(FILTER_ARG_COUNTS) do |name, values|
+      case name
+      when 'System'
+        jobs = jobs.by_system_name(values[0])
+        systems = systems.by_name(values[0])
+        @prev_filters << ['System', values]
+      when 'User'
+        jobs = jobs.by_user_name(values[0])
+        @prev_filters << ['User', values]
+      when 'Group'
+        jobs = jobs.by_group_name(values[0])
+        @prev_filters << ['Group', values]
+      when 'System type'
+        value = values[value_index]
+        sys_type = Bookie::Database::SystemType.find_by_name(value)
+        if sys_type
+          jobs = jobs.by_system_type(sys_type)
+          systems = systems.by_system_type(sys_type)
+        else
+          jobs = jobs.limit(0)
+          systems = systems.limit(0)
         end
+        value_index += 1
+        @prev_filters << ['System type', [value]]
+      when 'Time'
+        start_time_text = values[value_index]
+        end_time_text = values[value_index + 1]
+        begin
+          summmary_start_time = Time.parse(start_time_text)
+        rescue
+          flash[:error] = "Invalid start time '#{start_time_text}'"
+        end
+        begin
+          summary_end_time = Time.parse(end_time_text)
+        rescue
+          flash[:error] = "Invalid end time '#{end_time_text}'"
+        end
+        value_index += 2
+        @prev_filters << ['Time', [start_time_text, end_time_text]]
       end
     end
     
@@ -84,8 +77,6 @@ class JobsController < ApplicationController
     @systems_summary = systems.summary(summary_start_time, summary_end_time)
     
     #To be passed to the view
-    @filter_types = types
-    @filter_values = values
     @include_details = (params[:details] == "true")
     @page_start = PAGE_SIZE * (@page_num - 1)
     @page_end = @page_start + PAGE_SIZE
