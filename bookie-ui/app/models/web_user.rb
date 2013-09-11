f02b19f41aac0805448197e26035637f141d9f7b
Ben Merritt <blm768@gmail.com>
2013-09-11 14:05:29 -0700

    Started on authentication

---
+++ bookie-ui/app/models/web_user.rb
@@ -0,0 +1,6 @@
+class WebUser < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
--- bookie-ui/config/routes.rb
+++ bookie-ui/config/routes.rb
@@ -1,4 +1,5 @@
 BookieUi::Application.routes.draw do
+  devise_for :web_users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
