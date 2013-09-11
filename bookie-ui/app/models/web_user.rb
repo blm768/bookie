50f7b7ed253e47c1469e92772c50048f42ba0524
Ben Merritt <blm768@gmail.com>
2013-09-11 15:30:05 -0700

    Started work on requiring admin approval of account creation

--- bookie-ui/app/models/web_user.rb
+++ bookie-ui/app/models/web_user.rb
@@ -3,4 +3,18 @@ class WebUser < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #Based on https://github.com/plataformatec/devise/wiki/How-To:-Require-admin-to-activate-account-before-sign_in
+
+  def active_for_authentication?
+    super && approved?
+  end
+
+  def inactive_message
+    if !approved?
+      :not_approved
+    else
+      super
+    end
+  end
 end
