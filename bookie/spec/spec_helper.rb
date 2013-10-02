771d1b850850c4e9df03f5b8e72ed703f70736a2
Ben Merritt <blm768@gmail.com>
2013-10-02 14:57:05 -0700

    Cleaned up Helpers.create_summaries

--- bookie/spec/spec_helper.rb
+++ bookie/spec/spec_helper.rb
@@ -46,21 +46,20 @@ module Helpers
 
   #Creates summaries under diffferent conditions
   def create_summaries(obj, base_time)
-    start_time_1 = base_time
-    end_time_1   = base_time + 40.hours
-    #TODO: push this farther forward?
-    start_time_2 = base_time + 30.minutes
-    end_time_2 = base_time + 25.hours
+    base_start = base_time
+    base_end   = base_time + 40.hours
     summaries = {
       :all => obj.summary,
-      :all_constrained => obj.summary(start_time_1 ... end_time_1),
-      :clipped => obj.summary(start_time_2 ... end_time_2),
-      :empty => obj.summary(start_time_1 ... start_time_1),
+      :all_constrained => obj.summary(base_start ... base_end),
+      :wide => obj.summary(base_start - 1.hours ... base_end + 1.hours),
+      #TODO: push this farther forward?
+      :clipped => obj.summary(base_start + 30.minutes ... base_start + 25.hours),
+      :empty => obj.summary(base_start ... base_start),
     }
 
     #TODO: move?
     if obj.respond_to?(:by_command_name)
-      summaries[:all_filtered] = obj.by_command_name('vi').summary(start_time_1 ... end_time_1)
+      summaries[:all_filtered] = obj.by_command_name('vi').summary(base_start ... base_end)
     end
     
     summaries
