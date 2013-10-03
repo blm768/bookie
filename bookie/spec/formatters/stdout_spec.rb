c1a97fffad09ae8777ffdd86de7e5ad473dd2021
Ben Merritt <blm768@gmail.com>
2013-10-03 09:59:13 -0700

    Updated spec/formatters/stdout_spec.rb

--- bookie/spec/formatters/stdout_spec.rb
+++ bookie/spec/formatters/stdout_spec.rb
@@ -9,30 +9,28 @@ module Bookie
   end
 end
 
+include Bookie
+include Bookie::Database
+
 describe Bookie::Formatters::Stdout do
-  before(:all) do
-    @jobs = Bookie::Database::Job
-    @summaries = Bookie::Database::JobSummary
-  end
-  
+  let(:io_mock) { IOMock.new }
   before(:each) do
-    @m = IOMock.new
-    File.expects(:open).returns(@m)
-    @formatter = Bookie::Formatter.new(:stdout, 'mock.out')
+    File.expects(:open).at_least(0).returns(io_mock)
   end
-  
+  let(:formatter) { Formatter.new(:stdout, 'mock.out') }
+
   it "correctly opens files" do
     f = Bookie::Formatter.new(:stdout)
     f.instance_variable_get(:'@io').should eql STDOUT
     File.expects(:open).with('mock.out')
-    f = Bookie::Formatter.new(:stdout, 'mock.out')
+    Bookie::Formatter.new(:stdout, 'mock.out')
   end
   
   it "correctly formats jobs" do
     with_utc do
-    @formatter.print_jobs(@jobs.order(:start_time).limit(2))
-    @formatter.flush
-    @m.buf.should eql <<-eos
+      formatter.print_jobs(Job.order(:start_time).limit(2))
+      formatter.flush
+      io_mock.buf.should eql <<-eos
 User            Group           System               System type          Start time                 End time                   Wall time                      CPU time                       Memory usage         Command              Exit code  
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 root            root            test1                Standalone           2012-01-01 00:00:00        2012-01-01 01:00:00        0 weeks, 0 days, 01:00:00      0 weeks, 0 days, 00:01:40      200kb (avg)          vi                   0          
@@ -43,9 +41,9 @@ eos
     
   it "correctly formats summaries" do
     Time.expects(:now).returns(base_time + 40.hours).at_least_once
-    @formatter.print_summary(@jobs, @summaries, Bookie::Database::System)
-    @formatter.flush
-    @m.buf.should eql <<-eos
+    formatter.print_summary(Job, JobSummary, System)
+    formatter.flush
+    io_mock.buf.should eql <<-eos
 Number of jobs:               40
 Total CPU time:               0 weeks, 0 days, 01:06:40
 Successful:                   50.0000%
